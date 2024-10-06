7f68616d15f58e70d10530174e3055008d8b87b7
Sahil-Iqubal <sahiliqubal06@gmail.com>
2024-10-06 23:55:16 +0530

    Updated Doctors and Dashboard components with new functionality and imports

--- Dashboard/src/components/Dashboard.jsx
+++ Dashboard/src/components/Dashboard.jsx
@@ -1,9 +1,91 @@
-import React from 'react'
+import React, { useContext, useEffect, useState } from "react";
+import { Context } from "../main";
+import axios from "axios";
+import { Navigate } from "react-router-dom";
 
 const Dashboard = () => {
-  return (
-    <div></div>
-  )
-}
+  const { isAuthenticated, user } = useContext(Context);
+  const [appointments, setAppointments] = useState([]);
 
-export default Dashboard
+  useEffect(() => {
+    const fetchAppointments = async () => {
+      try {
+        const { data } = await axios.get(
+          "http://localhost:4000/api/v1/appointment/getall",
+          { withCredentials: true }
+        );
+        setAppointments(data.appointments);
+      } catch (error) {
+        setAppointments([]);
+        console.log("Some Error Occured While Fetching Appointments", error);
+      }
+    };
+    fetchAppointments();
+  }, []);
+
+  if (!isAuthenticated) {
+    return <Navigate to="/login" />;
+  }
+  return (
+    <>
+      <section className="dashboard page">
+        <div className="banner">
+          <div className="firstBox">
+            <img src="/doc.png" alt="docImg" />
+            <div className="content">
+              <div>
+                <p>Hello,</p>
+                <h5>{user && `${user.firstName} ${user.lastName}`}</h5>
+              </div>
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores
+                quibusdam amet eius similique fugiat nesciunt.
+              </p>
+            </div>
+          </div>
+          <div className="secondBox">
+            <p>Total Appointments</p>
+            <h3>1500</h3>
+          </div>
+          <div className="thirdBox">
+            <p>Registerd Doctors</p>
+            <h3>20</h3>
+          </div>
+        </div>
+        <div className="banner">
+          <h5>Appointments</h5>
+          <table>
+            <thead>
+              <tr>
+                <th>Patient</th>
+                <th>Date</th>
+                <th>Doctor</th>
+                <th>Department</th>
+                <th>Status</th>
+                <th>Visited</th>
+              </tr>
+            </thead>
+            <tbody>
+              {appointments && appointments.length > 0 ? (
+                appointments.map((appointment) => {
+                  return (
+                    <tr key={appointment._id}>
+                      <td>{`${appointment.firstName} ${appointment.lastName}`}</td>
+                      <td>{appointment.appointment_date.substring(0, 16)}</td>
+                      <td>{`${appointment.firstName} ${appointment.lastName}`}</td>
+                      <td>{`${appointment.firstName} ${appointment.lastName}`}</td>
+                    </tr>
+                  );
+                })
+              ) : (
+                <h1>NO APPOINTMENTS!</h1>
+              )}
+            </tbody>
+          </table>
+        </div>
+      </section>
+    </>
+  );
+};
+
+export default Dashboard;
