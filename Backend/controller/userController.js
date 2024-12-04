315592178fdc653bd7a97f96b47005e0243993de
Sahil-Iqubal <sahiliqubal06@gmail.com>
2024-12-04 22:09:32 +0530

    Renamed Frontend and updated utils, userController

--- Backend/controller/userController.js
+++ Backend/controller/userController.js
@@ -142,6 +142,8 @@ export const logoutAdmin = catchAsyncErrors(async (req, res, next) => {
     .cookie("adminToken", "", {
       httpOnly: true,
       expires: new Date(Date.now()),
+      secure: true,
+      sameSite: "None",
     })
     .json({
       success: true,
@@ -155,6 +157,8 @@ export const logoutPatient = catchAsyncErrors(async (req, res, next) => {
     .cookie("patientToken", "", {
       httpOnly: true,
       expires: new Date(Date.now()),
+      secure: true,
+      sameSite: "None",
     })
     .json({
       success: true,
